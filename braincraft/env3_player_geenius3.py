78a037e72da380e57a1a21d9420ffcf1322dec1c
Brian Kardon <brian.kardon@gmail.com>
2026-01-09 01:20:45 -0500

    Geenius3 14.67 ± 0.21

--- braincraft/env3_player_geenius3.py
+++ braincraft/env3_player_geenius3.py
@@ -110,23 +110,32 @@ class Model:
         # Initialize zeroed weight matrix
         self.Win  = np.zeros((n, 2*p+3))
         # Leftmost distance input to left neurons
-        self.Win[leftNeuronsIdx, leftCameraInputs] = amplitude*np.random.uniform(-1, 1, size=numNeuronsPerSide)
+        self.Win[0, 0] = amplitude*np.random.uniform(-1, 1) #, size=numNeuronsPerSide)
         # Rightmost distance input to neuron 1
-        self.Win[rightNeuronsIdx, rightCameraInputs] = amplitude*np.random.uniform(-1, 1, size=numNeuronsPerSide)
+        self.Win[numNeuronsPerSide-1, p-1] = amplitude*np.random.uniform(-1, 1) #, size=numNeuronsPerSide)
         # Add hit, energy, and bias connections
         # self.Win[:numSideNeurons, 2*p:2*p+3] = amplitude*np.random.uniform(-1, 1, size=[numSideNeurons, 3])
+        # Add bias connections
+        self.Win[:numSideNeurons, 2*p+2] = amplitude*np.random.uniform(-1, 1, size=numSideNeurons)
+
 
         ### INTERNAL WEIGHTS
         # Randomize internal connections
         self.W = np.zeros((n, n))
-        # # Interconnect left neurons
-        # self.W[0:numNeuronsPerSide, 0:numNeuronsPerSide] = amplitude*np.random.uniform(-1+weightBias, 1+weightBias, size=[numNeuronsPerSide, numNeuronsPerSide])
-        # # Interconnect right neurons
-        # self.W[numNeuronsPerSide:numSideNeurons, numNeuronsPerSide:numSideNeurons] = amplitude*np.random.uniform(-1+weightBias, 1+weightBias, size=[numNeuronsPerSide, numNeuronsPerSide])
+        # Interconnect left neurons
+        # self.W[range(numNeuronsPerSide), range(numNeuronsPerSide)] = amplitude*np.random.uniform(-1+weightBias, 1+weightBias, size=[1, numNeuronsPerSide])
+        # Interconnect right neurons
+        # self.W[range(numNeuronsPerSide, numSideNeurons), range(numNeuronsPerSide, numSideNeurons)] = amplitude*np.random.uniform(-1+weightBias, 1+weightBias, size=[1, numNeuronsPerSide])
+
+        # Chain connect left neurons
+        self.W[numNeuronsPerSide-1, list(range(1, numNeuronsPerSide))+[0]] = amplitude*np.random.uniform(-1+weightBias, 1+weightBias) #, size=[1, numNeuronsPerSide])
+        # Chain connect right neurons
+        self.W[numSideNeurons-1, list(range(numNeuronsPerSide+1, numSideNeurons))+[numNeuronsPerSide]] = amplitude*np.random.uniform(-1+weightBias, 1+weightBias) #, size=[1, numNeuronsPerSide])
+
         # Sparse connect left to right
-        # self.W[leftNeuronsIdx, rightNeuronsIdx] = amplitude*np.random.uniform(-1, 1, size=[1, numNeuronsPerSide])
+        self.W[leftNeuronsIdx, rightNeuronsIdx] = amplitude*np.random.uniform(-1, 1, size=[1, numNeuronsPerSide])
         # Sparse connect right to left
-        # self.W[rightNeuronsIdx, leftNeuronsIdx] = amplitude*np.random.uniform(-1, 1, size=[1, numNeuronsPerSide])
+        self.W[rightNeuronsIdx, leftNeuronsIdx] = amplitude*np.random.uniform(-1, 1, size=[1, numNeuronsPerSide])
 
         # Dense connect left and right
         # self.W[0:numNeuronsPerSide, numNeuronsPerSide:numSideNeurons] = amplitude*np.random.uniform(-1, 1, size=[1, numNeuronsPerSide])
@@ -148,6 +157,9 @@ def mutateNonzeroMatrix(matrix, changeFactors):
     nonzeros = matrix[matrix != 0]
     if numChanges == 0 or len(nonzeros) == 0:
         return
+    if numChanges > len(nonzeros):
+        numChanges = len(nonzeros)
+        changeFactors = changeFactors[:numChanges]
     mutateIdx = np.random.choice(range(len(nonzeros)), size=numChanges, replace=False)
     nonzeros[mutateIdx] += changeFactors
     matrix[matrix != 0] = nonzeros
@@ -180,55 +192,60 @@ def geenius_trainer():
     last_gen_best = 0
     learning_rate = 0
 
-    while True:
-        print('\nGeneration', gen)
-        generation = []
-        r = 0.1+(np.tanh(-2*learning_rate)+1)/2 #(15 - np.maximum(score_best, 1))/15
-        mc = 1.0
-        print('\nMutation rate:', r)
-        for i in range(populationSize):
-            # Evaluate model on 3 runs
-            if len(survivors) == 0:
-                # Must be first generation
-                child = Model(randomize=True)
-                if gen == 0 and i == 0:
-                    print('Num weights:', np.count_nonzero(child.Win), np.count_nonzero(child.W), np.count_nonzero(child.Wout))
-            else:
-                if i == 0 and model_best is not None:
-                    child = model_best.copy()
+    try:
+        while True:
+            print('\nGeneration', gen)
+            generation = []
+            r = 0.1+(np.tanh(-2*learning_rate)+1)/2 #(15 - np.maximum(score_best, 1))/15
+            mc = 0.1 + 2*(np.tanh(-2*learning_rate)+1)/2
+            print('\nMutation rate:', r)
+            print('Mutation size:', mc)
+            for i in range(populationSize):
+                # Evaluate model on 3 runs
+                if len(survivors) == 0:
+                    # Must be first generation
+                    child = Model(randomize=True)
+                    if gen == 0 and i == 0:
+                        print('Num weights:', np.count_nonzero(child.Win), np.count_nonzero(child.W), np.count_nonzero(child.Wout))
                 else:
-                    # Choose two parents from survivors weighted by their scores
-                    weights = np.array([m.fitness for m in survivors])
-                    weights = weights / np.sum(weights)
-                    parent1, parent2 = np.random.choice(survivors, p=weights, size=2)
-                    child = parent1.mate(parent2).mutate(changeRate=r, maxChange=mc)
+                    if i == 0 and model_best is not None:
+                        child = model_best.copy()
+                    else:
+                        # Choose two parents from survivors weighted by their scores
+                        weights = np.array([m.fitness for m in survivors])
+                        weights = weights / np.sum(weights)
+                        parent1, parent2 = np.random.choice(survivors, p=weights, size=2)
+                        child = parent1.mate(parent2).mutate(changeRate=r, maxChange=mc)
 
-            child.test()
-            generation.append(child)
+                child.test()
+                generation.append(child)
 
-            if child.score > score_best:
-                score_best = child.score
-                model_best = child.copy()
-                # print('\nbest Win :', model_best[0][model_best[0] != 0])
-                # print('best W   :', model_best[1][model_best[1] != 0])
-                # print('best Wout:', model_best[2][model_best[2] != 0])
-                print('\n' + str(model_best))
-                print('\n****** Overall best score:', score_best)
-            # Return best result so far
-            yield model_best.getComponents()
+                if child.score > score_best:
+                    score_best = child.score
+                    model_best = child.copy()
+                    # print('\nbest Win :', model_best[0][model_best[0] != 0])
+                    # print('best W   :', model_best[1][model_best[1] != 0])
+                    # print('best Wout:', model_best[2][model_best[2] != 0])
+                    print('\n' + str(model_best))
+                    print('\n****** Overall best score:', score_best)
+                # Return best result so far
+                yield model_best.getComponents()
 
-        # Sort by mean score, descending
-        generation.sort(reverse=True, key=lambda m:m.score)
-        survivors = [model for model in generation[:numSurvivors]]
-        # Assign fitness
-        selectionPressure = 2
-        [model.setFitness((numSurvivors - k)*selectionPressure) for k, model in enumerate(survivors)]
-        gen_best = max([m.score for m in generation])
-        learning_rate = gen_best - last_gen_best
-        print('\nGeneration best score:', gen_best, 'learning rate=', learning_rate)
-        last_gen_best = gen_best
+            # Sort by mean score, descending
+            generation.sort(reverse=True, key=lambda m:m.score)
+            survivors = [model for model in generation[:numSurvivors]]
+            # Assign fitness
+            selectionPressure = 2
+            [model.setFitness((numSurvivors - k)*selectionPressure) for k, model in enumerate(survivors)]
+            gen_best = max([m.score for m in generation])
+            learning_rate = gen_best - last_gen_best
+            print('\nGeneration best score:', gen_best, 'learning rate=', learning_rate)
+            last_gen_best = gen_best
 
-        gen = gen + 1
+            gen = gen + 1
+    except KeyboardInterrupt:
+        print('Skipping rest of training')
+        pass
 
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
@@ -243,7 +260,7 @@ if __name__ == "__main__":
 
     # Training (100 seconds)
     print(f"Starting training for 100 seconds (user time)")
-    model = train(geenius_trainer, timeout=100)
+    model = train(geenius_trainer, timeout=10000)
 
     seed = 12345
     np.random.seed(seed)
